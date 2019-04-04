e59d12a04743f1b5f7569b7adb9d91ba3b5470bb
Mike DeLong <mikedelong@outlook.com>
2019-04-04 12:03:37 -0400

    new file: a very basic frequency-score based method that does not account for sentence length.

---
+++ first_cut.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 12:13:06 2019
+
+@author: michael.delong
+"""
+
+import nltk.data
+
+from tika import parser
+
+from nltk import FreqDist
+from nltk import word_tokenize
+
+import matplotlib.pyplot as plt
+
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+input_file = './data/911-commission-full-report.pdf'
+parsed = parser.from_file(input_file)
+
+print('our content is a string of length {}'.format(len(parsed['content'])))
+content = parsed['content']
+
+sentences = tokenizer.tokenize(content)
+print('before we remove very short sentences we have {} sentences'.format(len(sentences)))
+sentences = [item for item in sentences if len(item) > 1]
+print('after we remove very short sentences we have {} sentences'.format(len(sentences)))
+
+# now we need to count words and do a frequency score for each word
+
+freq_dist = FreqDist()
+for sentence in sentences:
+    for word in word_tokenize(sentence):
+        freq_dist[word.lower()] += 1
+
+word_count = freq_dist.N()
+print('our frequency distribution has {} items'.format(len(freq_dist)))
+print('our word count is {}'.format(freq_dist.N()))
+
+# now let's traverse our frequency distribution and build our token-probability map
+probabilities = {
+        word : float(freq_dist[word])/float(word_count) for word in freq_dist}
+
+# now we are ready to calculate scores for each sentence
+sentence_scores = {
+        sentence : sum([probabilities[word.lower()] for word in word_tokenize(sentence)])
+        for sentence in sentences}
+
+candidates = {key: value for key, value in sentence_scores.items() if value > 3.0 }
+
+for key, value in candidates.items():
+    print('{:5.2f} {}'.format(value, key[:100]))
+    
+values = [value for value in candidates.values()]
+plt.plot(sorted(values))
+plt.show()
