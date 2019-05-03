93d0ce1464a7ad0ff71b739877ba7a5f340ac27a
Mike DeLong <mikedelong@outlook.com>
2019-05-03 08:18:58 -0400

    feature: use a real corpus, in this case Emma by Jane Austen. feature: add a list of the file IDs we have in NLTK/Gutenberg for future work

--- tf_idf_based.py
+++ tf_idf_based.py
@@ -121,15 +121,19 @@ def rank_sentences(doc, doc_matrix, feature_names, top_n=3):
     return ranked_sents[:top_n]
 
 
+gutenberg_file_ids = ['austen-emma.txt', 'austen-persuasion.txt', 'austen-sense.txt', 'bible-kjv.txt',
+                      'blake-poems.txt', 'bryant-stories.txt', 'burgess-busterbrown.txt',
+                      'carroll-alice.txt', 'chesterton-ball.txt', 'chesterton-brown.txt',
+                      'chesterton-thursday.txt', 'edgeworth-parents.txt', 'melville-moby_dick.txt',
+                      'milton-paradise.txt', 'shakespeare-caesar.txt', 'shakespeare-hamlet.txt',
+                      'shakespeare-macbeth.txt', 'whitman-leaves.txt']
+
 if __name__ == '__main__':
     t0 = time()
     # todo: add data pickle
     # todo: move this data file name to a settings file
     # Load corpus data used to train the TF-IDF Transformer
-    # data = pickle.load(open('./data/data.pkl', 'rb'))
-    # data = list(nltk.corpus.gutenberg.words('austen-emma.txt'))
-    # todo use a real corpus here
-    data = ['this and that', 'the other thing']
+    data = list(nltk.corpus.gutenberg.words('austen-emma.txt'))
 
     # Load the document you wish to summarize
     title = ''
