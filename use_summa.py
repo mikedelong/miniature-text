7dfa48c1c19fa71ea4d70c33ac92c70a4e305568
Mike DeLong <mikedelong@outlook.com>
2019-05-21 08:41:48 -0400

    feature: switch to reading and summarizing an external text file.

--- use_summa.py
+++ use_summa.py
@@ -1,46 +1,13 @@
 from time import time
 
+from summa import keywords
 from summa import summarizer
 
-text = """must be controlled in real time. Examples include systems \
-that interact with humans and robotic systems, such as autonomous \
-vehicles. In this paper, we address real-time planning, \
-where the planner must return the next action for the \
-system to take within a specified wall-clock time bound. \
-Providing real-time heuristic search algorithms that are \
-complete in domains with dead-end states is a challenging \
-problem. Traditional real-time planners are inherently incomplete \
-due to the limited time available to make a decision \
-even when the state space is fully observable and the actions \
-are deterministic. Cserna et al. (2018) proposed the first realtime \
-heuristic search method, SafeRTS, that is able to reliably \
-reach a goal in domains with dead-ends. Prior real-time \
-methods focus their search effort on a single objective that \
-minimizes the cost to reach the goal. A single objective is \
-insufficient to provide completeness and minimize the time \
-to reach the goal as these often contradict each other. Thus, \
-SafeRTS distributes the available time between searches optimizing \
-the independent objectives of safety and finding the goal. \
-The contribution of this work is four-fold. First, we argue \
-that benchmark domains used for real-time planning \
-may not be good indicators of performance in the context \
-of safe real-time planning. We present a new set of benchmarks \
-that overcome the deficiencies of previous benchmark \
-domains. Second, we show how to utilize meta information \
-presented by safety oriented real-time search methods to reduce \
-redundant expansions during both the safety and goal-oriented \
-searches. This improvement marginally reduces the \
-goal achievement time (GAT) while it does not increase \
-the space and time complexity of the safe real-time search \
-method. Third, we prove inefficiencies in the approach taken \
-by SafeRTS by examining properties of local search spaces \
-and the changing priority of which nodes to prove safe as \
-an LSS grows. Lastly, we introduce a new framework for \
-safe real-time heuristic search that utilizes the time bound \
-unique to real-time planning. This framework follows the \
-same basic principle of search effort distribution as SafeRTS \
-but does so more efficiently.We empirically demonstrate the \
-potential of the new framework."""
+with open('./data/clean-1905.03298.txt', 'r') as input_fp:
+    lines = input_fp.read().splitlines()
+
+text = ' '.join(lines)
+
 
 for ratio in range(1, 11):
     float_ratio = float(ratio) / 10.0
@@ -53,5 +20,7 @@ for ratio in range(1, 11):
     time_after = time()
     print('summarize takes {:5.4f}s for ratio {:5.2f} and summary has length {}'.format(time_after - time_before,
                                                                                         float_ratio, len(summary)))
+
+print('keywords: {}'.format(keywords.keywords(text=text)))
 
 quit(0)
